5efc825803ebb0877aa9d1098b901791e37417e1
anirban2005143a <23je0104@iitism.ac.in>
2025-03-06 15:41:01 +0530

    delete googlr auth from backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 const app = express();
-import session from "express-session";
-import passport from "passport";
 
 app.use(
   cors({
@@ -23,21 +21,9 @@ app.use(
   })
 );
 
-app.use(session({
-  secret: 'subhambhai',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false  }
-}))
-
-app.use(passport.initialize())
-app.use(passport.session())
-
 import userRouter from "./routes/user.routes.js";
-import googleAuthentication from "./auth/googleauth.js";
 
 app.use("/api/haxplore/user",userRouter);
-app.use("/auth",googleAuthentication);
 
 
 app.use("/" , (req , res) => {
